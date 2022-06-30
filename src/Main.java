522e6085cf4f77b01bd85ba87e0c1b5fdbf39715
JuanK81 <jcasalsbadolato@gmail.com>
2022-06-30 11:58:49 +0200

    create Persona class, create persona instance

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,52 @@
 public class Main {
     public static void main(String[] args) {
 
-            }
+        Persona persona = new Persona();
+
+        persona.setEdad(32);
+        persona.setNombre("Paquito");
+        persona.setTelefono(999666555);
+
+        int edad = persona.getEdad();
+        String nombre = persona.getNombre();
+        int telefono = persona.getTelefono();
+
+        System.out.println(edad + " " + nombre + " " + telefono);
+
+    }
+
+    static class Persona {
+        private int edad;
+        private String nombre;
+        private int telefono;
+
+
+        public void setEdad(int edad) {
+            this.edad = edad;
         }
+
+        public int getEdad() {
+            return this.edad;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getNombre() {
+            return this.nombre;
+        }
+
+        public void setTelefono(int telefono) {
+            this.edad = telefono;
+        }
+
+        public int getTelefono() {
+            return this.telefono;
+        }
+
+    }
+}
 
 
 
